6a29f34ac3bd98e14c72749f8cc5015fb7dbaf9d
Harish Sharma <harish.zetetic@gmail.com>
2024-03-19 13:07:53 +0530

    useRef use case 1

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import UseRefHook from './components/Hooks/UseRefHook';
+
+function App() {
+  return (<div className='App'>
+    <UseRefHook />
+    </div>);
+}
+
+export default App;
