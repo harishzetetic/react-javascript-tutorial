23376d30d2c1e956d5812cacba41cb0f5a5470a4
Harish Sharma <harish.zetetic@gmail.com>
2024-03-21 17:02:03 +0530

    prevState value with useRef Hook

--- src/components/Hooks/UseRefHook.js
+++ src/components/Hooks/UseRefHook.js
@@ -2,19 +2,15 @@ import { useRef, useState, useEffect } from 'react';
 
 const UseRefHook = () => {
     const [text, setText] = useState('')
-    const [renderCount, setRenderCount] = useState(0)
-    
-    const renderCountRef = useRef(0)
-    
-    useEffect(()=>{
-        // setRenderCount(prev => prev + 1)
-        renderCountRef.current = renderCountRef.current + 1 
-    })
+    const prevStateValue = useRef('')
 
+    useEffect(()=>{
+        prevStateValue.current = text
+    }, [text])
     return <>
     <h1>useRef() Hook</h1>
     <input type='text' value={text} onChange={e=>setText(e.target.value)}/>
-    <h3>Total render count is {renderCountRef.current}</h3>
+    <h4>Current State Value is {text} and prev was {prevStateValue.current}</h4>
     </>
 }
 
