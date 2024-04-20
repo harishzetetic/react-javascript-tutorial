a45ff160dc74dbb35a6002af31f4de507c9770c4
Harish Sharma <harish.zetetic@gmail.com>
2024-04-20 15:24:09 +0530

    first commit

--- src/components/customHooks/useArray.js
+++ src/components/customHooks/useArray.js
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 export default function useArray(defaultValue){
     const [array, setArray] = useState(defaultValue);
-
-    function push(element){
+    
+    function push(element){ 
         setArray(a=>[...a, element])
     }
     function filter(callback){
