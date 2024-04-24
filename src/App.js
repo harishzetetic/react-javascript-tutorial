b6919dd2f9efb8776bbc7468cc36c8276697f4e9
Harish Sharma <harish.zetetic@gmail.com>
2024-04-24 06:31:04 +0530

    useStateWihHstory and usePrev

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { useState } from 'react';
 import './App.css';
-import useArray from './components/customHooks/useArray';
-
+import usePrevious from './components/customHooks/usePrevious';
+import useStateWithHistory from './components/customHooks/useStateWithHistory';
+ 
 function App() {
-  const {array, set, push, remove, filter, update, clear} = useArray([1,2,3,4,5,6,7]);
+  const [count, setCount, {history, pointer, back, forward, go}] = useStateWithHistory(1);
+  const [name, setName] = useState('Known')
+
+
   return (<div className='App'>
-    <h1>Custom Hook | useArray</h1>
-    {array.join(', ')}
-    <br/>
-    <button onClick={() => push(8)}>Push</button>
-    <button onClick={()=>update(1,9)}>Update 2nd index element</button>
-    <button onClick={()=>remove(1)}>Remove 2nd Element</button>
-    <button onClick={()=>clear()}>Clear</button>
+    <h1>Custom Hook | useStateWithHistory</h1>
+    <div>{count} </div>
+    <div>{history.join(', ')}</div>
+    <div>Pointer - {pointer}</div>
+    <div>{name}</div>
+
+    <button onClick={()=>setCount(prev=>prev*2)}>Double</button>
+    <button onClick={()=>setCount(prev=>prev+1)}>Incremetn</button>
+    <button onClick={back}>Back</button>
+    <button onClick={forward}>Forward</button>
+    <button onClick={()=>go(2)}>Go To 2nd Index</button>
+    <button onClick={()=>setName('Technical')}>Change Name</button>
     </div>);
 }
 
